299b49bea0ec02fb871db538f6ce6f6c7daa33e9
Pattyjohn <juanin_92@msn.com>
2016-03-09 12:27:31 +0100

    Primer Commit
    Creación del proyecto.

---
+++ CovaEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Juan\Desktop\Cova\CovaEnv\Scripts\python2.7.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ README.md
@@ -0,0 +1,2 @@
+# Cova
+Proyecto Cova para el seguimiento de pacientes crónicos.
